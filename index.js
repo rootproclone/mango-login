27db4f335849ef322f4d5ee35d88dc4fbe88f23e
rootproclone <146494827+rootproclone@users.noreply.github.com>
2023-09-30 20:47:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Connect to MongoDB
 mongoose.connect("mongodb+srv://sadamdon1234:1YktFZRZ1cX0PRj4@cluster0.nhacelr.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 mongoose.connection.on('error', (error) => console.error('MongoDB connection error:', error));
 
-// Define Mongoose models
 const agencySchema = new mongoose.Schema({
   name: { type: String, required: true },
   address1: { type: String, required: true },
@@ -36,26 +34,25 @@ const Client = mongoose.model('Client', clientSchema);
 
 app.use(bodyParser.json());
 
-const secretKey = 'mn1f4mfulKNrMZ0aAqbrw';
+const secretKey = process.env.SECRET_KEY || 'mn1f4mfulKNrMZ0aAqbrw';
 
-const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  console.log('Received token:', token);
+const authenticateToken = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization');
+    if (!token) return res.sendStatus(401);
 
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token.split(' ')[1], secretKey, (err, user) => {
-    if (err) {
-      if (err.name === 'TokenExpiredError') {
-        return res.status(401).json({ message: 'Token has expired' });
-      }
-      console.error(err);
-      return res.status(403).json({ message: 'Invalid token' });
-    }
+    const user = await jwt.verify(token.split(' ')[1], secretKey);
     req.user = user;
     next();
-  });
+  } catch (error) {
+    next(error);
+  }
 };
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ success: false, error: 'Internal Server Error' });
+});
 
 const generateToken = (user) => {
   const token = jwt.sign(user, secretKey, { expiresIn: '15m' });
@@ -76,9 +73,9 @@ app.post('/api/get-new-token', (req, res) => {
   const token = authenticateUser(username, password);
 
   if (token) {
-    res.status(200).json({ token });
+    res.status(200).json({ success: true, token });
   } else {
-    res.status(401).json({ error: 'Invalid credentials' });
+    res.status(401).json({ success: false, error: 'Invalid credentials' });
   }
 });
 
@@ -86,23 +83,30 @@ app.post('/api/create-agency', authenticateToken, async (req, res) => {
   try {
     const agencyData = req.body;
     const newAgency = await Agency.create(agencyData);
-    res.status(200).json({ agency: newAgency });
+    res.status(200).json({ success: true, agency: newAgency });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ success: false, error: 'Server error' });
   }
 });
 
-app.post('/api/agency-client', authenticateToken, async (req, res) => {
+app.post('/api/agency/:agencyId/client', authenticateToken, async (req, res) => {
   try {
-    const { agency, client } = req.body;
-    const newAgency = await Agency.create(agency);
-    client.agencyId = newAgency._id;
-    const newClient = await Client.create(client);
-    res.status(200).json({ agency: newAgency, client: newClient });
+    const agencyId = req.params.agencyId; 
+    const clients = req.body.clients;
+
+    const newClients = [];
+
+    for (const client of clients) {
+      client.agencyId = agencyId;
+      const newClient = await Client.create(client);
+      newClients.push(newClient);
+    }
+
+    res.status(200).json({ success: true, clients: newClients });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ success: false, error: 'Server error' });
   }
 });
 
@@ -116,35 +120,34 @@ app.put('/api/client/:clientId', authenticateToken, async (req, res) => {
       { new: true }
     );
     if (!updatedClient) {
-      return res.status(404).json({ error: 'Client not found' });
+      return res.status(404).json({ success: false, error: 'Client not found' });
     }
-    res.status(200).json({ message: 'Update Successfully' });
+    res.status(200).json({ success: true, message: 'Update Successful' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ success: false, error: 'Server error' });
   }
 });
 
 app.get('/api/top-clients', authenticateToken, async (req, res) => {
-    try {
-      const topClients = await Client.find()
-        .sort({ totalBill: -1 })
-        .limit(5)
-        .populate('agencyId', 'name'); // Populate the agencyId field with the name property
-  
-      const result = topClients.map((client) => ({
-        AgencyName: client.agencyId.name,
-        ClientName: client.name,
-        TotalBill: client.totalBill,
-      }));
-  
-      res.status(200).json(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
-  
+  try {
+    const topClients = await Client.find()
+      .sort({ totalBill: -1 })
+      .limit(5)
+      .populate('agencyId', 'name');
+
+    const result = topClients.map((client) => ({
+      AgencyName: client.agencyId.name,
+      ClientName: client.name,
+      TotalBill: client.totalBill,
+    }));
+
+    res.status(200).json({ success: true, result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Server error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
